a8291df841da4169335f00c8f294dadb776b317b
SamuelChing <sam.98Panton@gmail.com>
2019-11-15 20:52:52 -0600

    '151119'

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,5 +11,27 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  getUser(){    
+    console.log("Entro");
+    fetch("http://localhost:3000/users/amairena", {
+        "method": "GET"        
+      })
+      .then(response => {
+        if(response.ok){
+            console.log(response);
+            return response.json();
+        }else{
+            throw new Error('BAD HTTP stuff');
+        }
+        console.log(response);
+      })
+      .then( (jsonData) =>{
+        console.log(jsonData);
+        //Nombre de la función a la que quiere retornar el jsonData, ya que no se puede con el return
+        //FiltroCiudad(jsonData);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
 }
