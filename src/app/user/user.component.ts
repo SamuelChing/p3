233d37fe04575ca9c896d51aa062d9d18726e518
Akuseru3 <axel2799@hotmail.com>
2019-11-21 04:55:44 -0600

    'macumbaxel'

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -73,7 +73,7 @@ export class UserComponent implements OnInit {
 
   ngOnInit() {
      this.userLogged=this.route.snapshot.paramMap.get('id');
-     
+     this.getData();
   }
   fixDate(date:Date){
     var dateResult= (date.getFullYear()+"-"+(date.getMonth()+1)+"-"+(date.getDate()));
@@ -170,6 +170,515 @@ export class UserComponent implements OnInit {
     this.software.push(this.createSoftware(this.pSoftware,this.pSoftwareType));
     this.dataSourceSoftware = new MatTableDataSource(this.software);
     
+  }
+  getWorks(userPK){   
+    //console.log("Entra a works") 
+    fetch("http://localhost:3000/userExperience/"+userPK,{
+        "method": "GET"        
+      })
+      .then(response => {
+        if(response.ok){
+            //console.log(response);
+            return response.json();
+        }else{
+            throw new Error('BAD HTTP stuff');
+        }
+      })
+      .then( (jsonData) =>{        
+        
+        console.log(jsonData);                
+        jsonData.forEach(element => {        
+          this.work.push({company:element.company,job:element.role,
+            date1:element.admissiondate,date2:element.departuredate,description:element.description,active:element.activejob});
+          this.dataSourceWork = new MatTableDataSource(this.work);
+        });
+        //Nombre de la función a la que quiere retornar el jsonData, ya que no se puede con el return
+        //FiltroCiudad(jsonData);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  
+  getLanguages(userPK){   
+    //console.log("Entra a works") 
+    fetch("http://localhost:3000/newLanguage/1/"+userPK,{
+        "method": "GET"        
+      })
+      .then(response => {
+        if(response.ok){
+            //console.log(response);
+            return response.json();
+        }else{
+            throw new Error('BAD HTTP stuff');
+        }
+      })
+      .then( (jsonData) =>{        
+        
+        console.log(jsonData);                
+        jsonData.forEach(element => { 
+          this.languages.push({name:element.language,domain:element.proficiency});
+          this.dataSourceLanguage = new MatTableDataSource(this.languages);
+        });
+        //Nombre de la función a la que quiere retornar el jsonData, ya que no se puede con el return
+        //FiltroCiudad(jsonData);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  getSoftware(userPK){   
+    //console.log("Entra a works") 
+    fetch("http://localhost:3000/newSoftware/1/"+userPK,{
+        "method": "GET"        
+      })
+      .then(response => {
+        if(response.ok){
+            //console.log(response);
+            return response.json();
+        }else{
+            throw new Error('BAD HTTP stuff');
+        }
+      })
+      .then( (jsonData) =>{        
+        
+        console.log(jsonData);                
+        jsonData.forEach(element => {           
+          this.software.push(this.createSoftware(element.softwarename,element.softwaretype));
+          this.dataSourceSoftware = new MatTableDataSource(this.software);
+        });
+        //Nombre de la función a la que quiere retornar el jsonData, ya que no se puede con el return
+        //FiltroCiudad(jsonData);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  getCertifications(userPK){   
+    //console.log("Entra a works") 
+    fetch("http://localhost:3000/newCertification/1/"+userPK,{
+        "method": "GET"        
+      })
+      .then(response => {
+        if(response.ok){
+            //console.log(response);
+            return response.json();
+        }else{
+            throw new Error('BAD HTTP stuff');
+        }
+      })
+      .then( (jsonData) =>{        
+        
+        console.log(jsonData);                
+        jsonData.forEach(element => { 
+          this.certificaciones.push({title:element.title,name:element.certificationgiver,year:element.certificationyear});
+          this.dataSourceCertification = new MatTableDataSource(this.certificaciones);
+        });
+        //Nombre de la función a la que quiere retornar el jsonData, ya que no se puede con el return
+        //FiltroCiudad(jsonData);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  getStudies(userPK){   
+    console.log("Entra a works",userPK) 
+    fetch("http://localhost:3000/userStudies/"+userPK,{
+        "method": "GET"        
+      })
+      .then(response => {
+        if(response.ok){
+            //console.log(response);
+            return response.json();
+        }else{
+            throw new Error('BAD HTTP stuff');
+        }
+      })
+      .then( (jsonData) =>{        
+        
+        console.log(jsonData);                
+        jsonData.forEach(element => { 
+          this.studies.push({grade:element.grade,name:element.titlegiver,year:element.graduated});
+          this.dataSourceStudies = new MatTableDataSource(this.studies);
+        });
+        //Nombre de la función a la que quiere retornar el jsonData, ya que no se puede con el return
+        //FiltroCiudad(jsonData);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  getUserPK(){
+    console.log("Usuario:",this.userLogged)
+    fetch("http://localhost:3000/users/"+1+"/"+this.userLogged, {
+        "method": "GET"        
+      })
+      .then(response => {
+        if(response.ok){
+            //console.log(response);
+            return response.json();
+        }else{
+            throw new Error('BAD HTTP stuff');
+        }
+      })
+      .then( (jsonData) =>{
+        console.log(jsonData);
+        this.getWorks(jsonData[0].userid);
+        this.getLanguages(jsonData[0].userid);
+        this.getSoftware(jsonData[0].userid);
+        this.getCertifications(jsonData[0].userid);
+        this.getStudies(jsonData[0].userid);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  
+  makePostLanguages(type,username,userID){
+    //this.getID(username,type);      
+    this.languages.forEach(async function(element){
+      var language={
+        "person":userID,
+        "language":element.name,
+        "proficiency":element.domain
+      }
+      await fetch("http://localhost:3000/newLanguage/:"+type, {
+          "method": "POST",
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            },
+            body: JSON.stringify(language)
+        }).then(function(response) {
+          //console.log(response);
+          if(response.status ==201){            
+            console.log("Language added correctly");            
+          }else{            
+            alert("Error adding this user languages");
+          }
+        });   
+    });
+  }
+  
+  makePostCertifications(type,username,userID){
+    console.log(this.certificaciones)
+    this.certificaciones.forEach(async function(element){
+      var language={}    
+      language ={
+        "person":userID,
+        "title":element.title,
+        "certificationGiver":element.title,
+        "certificationYear":element.year     
+      }
+      console.log(language)
+      await fetch("http://localhost:3000/newCertification/:"+type, {
+          "method": "POST",
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            },
+            body: JSON.stringify(language)
+        }).then(function(response) {
+          //console.log(response);
+          if(response.status ==201){            
+            console.log("Certification added correctly");            
+          }else{            
+            alert("Error adding this user certifications");
+          }
+        });   
+    });
+  }
+  makePostSoftware(type,username,userID){
+    this.software.forEach(async function(element){
+      var language={}    
+      language ={
+        "person":userID,
+        "softwareType":element.type,
+        "softwareName":element.name     
+      }
+      await fetch("http://localhost:3000/newSoftware/:"+type, {
+          "method": "POST",
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            },
+            body: JSON.stringify(language)
+        }).then(function(response) {
+          //console.log(response);
+          if(response.status ==201){            
+            console.log("Software added correctly");            
+          }else{            
+            alert("Error adding this user softwares");
+          }
+        });   
+    });
+  }
+  makePostExperience(type,username,userID){
+    this.work.forEach(async function(element){
+      var language={}    
+      language ={
+        "person":userID,
+        "company":element.company,
+        "role":element.job,
+        "admissionDate":element.date1,
+         "departureDate":element.date2,
+         "activeJob":element.active,
+         "description":element.description
+      }
+      await fetch("http://localhost:3000/userExperience", {
+          "method": "POST",
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            },
+            body: JSON.stringify(language)
+        }).then(function(response) {
+          //console.log(response);
+          if(response.status ==201){            
+            console.log("Experience added correctly");            
+          }else{            
+            alert("Error adding this user Experience");
+          }
+        });   
+    });
+  }
+  makePostStudies(type,username,userID){
+    this.studies.forEach(async function(element){
+      var studies={}    
+      studies ={
+        "person":userID,
+        "grade":element.grade,
+        "titleGiver":element.name,
+        "graduated":element.year     
+      }
+      await fetch("http://localhost:3000/userStudies", {
+          "method": "POST",
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            },
+            body: JSON.stringify(studies)
+        }).then(function(response) {
+          //console.log(response);
+          if(response.status ==201){            
+            console.log("Studies added correctly");            
+          }else{            
+            alert("Error adding this user Studies");
+          }
+        });   
+    });
+  }
+  async deletePostSoftware(userID){
+    //this.getID(username,type);
+    await fetch("http://localhost:3000/newSoftware/1/"+userID, {
+        "method": "DELETE",
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+          }
+      }).then(function(response) {
+        //console.log(response);
+        if(response.status ==201){            
+          console.log("Software deleted correctly");            
+        }else{            
+          alert("Error deleting this user languages");
+        }
+      });
+  }
+  async deletePostLanguage(userID){
+    //this.getID(username,type);
+    await fetch("http://localhost:3000/newLanguage/1/"+userID, {
+        "method": "DELETE",
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+          }
+      }).then(function(response) {
+        //console.log(response);
+        if(response.status ==201){            
+          console.log("Language deleted correctly");            
+        }else{            
+          alert("Error deleting this user languages");
+        }
+      });
+  }
+  async deletePostCertifications(userID){
+    //this.getID(username,type);
+    await fetch("http://localhost:3000/newCertification/1/"+userID, {
+        "method": "DELETE",
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+          }
+      }).then(function(response) {
+        //console.log(response);
+        if(response.status ==201){            
+          console.log("Certification deleted correctly");            
+        }else{            
+          alert("Error deleting this user languages");
+        }
+      });
+  }
+  async deletePostExperience(userID){
+    //this.getID(username,type);
+    await fetch("http://localhost:3000/userExperience/"+userID, {
+        "method": "DELETE",
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+          }
+      }).then(function(response) {
+        //console.log(response);
+        if(response.status ==201){            
+          console.log("Experience deleted correctly");            
+        }else{            
+          alert("Error deleting this user languages");
+        }
+      });
+  }
+  async deletePostStudies(userID){
+    //this.getID(username,type);
+    await fetch("http://localhost:3000/userStudies/"+userID, {
+        "method": "DELETE",
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+          }
+      }).then(function(response) {
+        //console.log(response);
+        if(response.status ==201){            
+          console.log("Studies deleted correctly");            
+        }else{            
+          alert("Error deleting this user languages");
+        }
+      });
+  }
+  
+  
+
+  getData(){
+    fetch("http://localhost:3000/newUserData/1/"+'Akuseru',{
+        "method": "GET"        
+      })
+      .then(response => {
+        if(response.ok){
+            //console.log(response);
+            return response.json();
+        }else{
+            throw new Error('BAD HTTP stuff');
+        }
+      })
+      .then( (jsonData) =>{
+        
+        
+        var user = jsonData[0]
+        //console.log(user);                
+        this.firstName = user.firstname
+        this.lastName = user.lastname
+        this.country = user.nationality
+        this.birthday = user.birthday
+        this.province = user.province
+        this.canton = user.canton
+        this.district =user.district
+        this.url =user.picture
+        this.email =user.mail
+        this.phone =user.phone
+        this.webSite =user.website
+        this.getUserPK()
+        //Nombre de la función a la que quiere retornar el jsonData, ya que no se puede con el return
+        //FiltroCiudad(jsonData);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  makePost(type,userData,userid){
+    console.log(userData);
+    fetch("http://localhost:3000/newUserData/:"+type, {
+        "method": "PUT",
+        headers : {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+          },
+          body: JSON.stringify(userData)
+      }).then(async (response)=> {
+        //console.log(response);
+        if(response.status ==201){          
+          alert("User updated correctly"); 
+          await this.deletePostLanguage(userid);
+          await this.deletePostSoftware(userid);
+          await this.deletePostCertifications(userid);
+          await this.deletePostExperience(userid);
+          await this.deletePostStudies(userid);
+          console.log("-------------------------------------------")
+          //
+          this.makePostLanguages(type,this.userLogged,userid);
+          this.makePostSoftware(type,this.userLogged,userid);
+          this.makePostCertifications(type,this.userLogged,userid);
+          this.makePostExperience(type,this.userLogged,userid);
+          this.makePostStudies(type,this.userLogged,userid);         
+
+          window.location.reload();
+        }else{
+          alert("Error adding this user data");
+        }
+      });     
+  }
+  addUserInfo(){
+    var userData;    
+    if(this.firstName && this.lastName && this.country && this.province && this.canton && this.district 
+      && this.email && this.phone && this.webSite && this.url){
+      console.log("Usuario:",this.userLogged)
+      fetch("http://localhost:3000/users/"+1+"/"+this.userLogged, {
+          "method": "GET"        
+        })
+        .then(response => {
+          if(response.ok){
+              //console.log(response);
+              return response.json();
+          }else{
+              throw new Error('BAD HTTP stuff');
+          }
+        })
+        .then( (jsonData) =>{                   
+          userData={          
+          "firstName":this.firstName,
+          "LastName":this.lastName,
+          "nationality":this.country,
+          "province":this.province,
+          "canton":this.canton,
+          "district":this.district,
+          "mail":this.email,
+          "phone":this.phone,
+          "website":this.webSite,
+          "picture":this.url,                
+          "username":this.userLogged
+        }
+        this.makePost(1,userData,jsonData[0].userid);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+      
+      
+    }else{
+      alert("Please fill the all the user information")
+    }       
+  }
+
+  addNewUser(){   
+     
+    if((this.firstName && this.lastName && this.country && this.province && this.canton && this.district 
+      && this.email && this.phone && this.webSite && this.url && (this.languages) || this.languages.length)
+      && (this.software || this.software.length) && (this.certificaciones || this.certificaciones.length)
+      && (this.work || this.work.length)){
+       
+      this.addUserInfo();
+      
+      
+    }else{
+      alert("Check your and fill all the information");
+    }
   }
 
 }
